4fd3c02cb974b0e14531a71cc53be058964963c5
jang walia <jangiwalia83@gmail.com>
2022-01-10 12:52:52 -0800

    add Group BY and Having queries

---
+++ 3_queries_groupBY/average_duration.sql
@@ -0,0 +1,8 @@
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignement_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignement_duration DESC;
+
+
