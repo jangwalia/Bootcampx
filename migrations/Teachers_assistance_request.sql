1aa447652bacbf749e979db510ed271cb54dc091
jang walia <jangiwalia83@gmail.com>
2022-01-12 05:56:11 -0800

    implement queries for assistance requests

--- migrations/Teachers_assistance_request.sql
+++ migrations/Teachers_assistance_request.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Teachers(
+CREATE TABLE teachers(
 id SERIAL PRIMARY KEY NOT NULL,
 name VARCHAR(255) NOT NULL,
 start_date DATE,
@@ -10,10 +10,10 @@ CREATE TABLE assistance_requests(
 id SERIAL PRIMARY KEY NOT NULL,
 assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
 student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-teacher_id INTEGER REFERENCES Teachers(id) ON DELETE CASCADE,
+teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
 created_at TIMESTAMP,
 started_at TIMESTAMP,
 completed_at TIMESTAMP,
-students_feedback TEXT,
+student_feedback TEXT,
 teacher_feedback TEXT
 );
